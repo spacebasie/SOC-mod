719675cb89ca102ee4589b37644f720f78b3a458
frilis <frilis@umich.edu>
2022-11-25 20:37:33 -0500

    Added determination coeff

--- loadin.py
+++ loadin.py
@@ -4,7 +4,7 @@ from sklearn.model_selection import train_test_split
 from sklearn.pipeline import make_pipeline
 from sklearn.ensemble import RandomForestRegressor
 from sklearn.preprocessing import StandardScaler
-from sklearn.metrics import mean_absolute_error, mean_squared_error
+from sklearn.metrics import mean_absolute_error, mean_squared_error, f1_score, r2_score
 
 
 
@@ -22,7 +22,7 @@ def RFreg_test(X_train, X_test, y_train, y_test):
     y_pred = pipeline.predict(X_test)
     print("Mean Squared Error is: %.2f"%(mean_squared_error(y_test, y_pred)))
     print("Mean Absolute Error is: %.2f"%(mean_absolute_error(y_test, y_pred)))
-    print("")
+    print("Determination Coefficient R2 is: %.2f"%(r2_score(y_test, y_pred)))
     return 0
 
 
@@ -56,7 +56,5 @@ def main():
 
     """PART 5 - TBD: IF ENOUGH TIME AT HAND: REPEAT TESTING IN A DIFFERENT DATASET AND COMPARE PERFORMANCE"""
 
-    # This a test
-    # new
 if __name__ == "__main__":
     main()
